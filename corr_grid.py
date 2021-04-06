7abf9f5bba30ab9c6daf8fdca53c37cd03d3dd1e
AstroJosePC <rayzote@email.arizona.edu>
2021-04-06 15:16:24 -0500

    :bug: proper parameter parsing

--- corr_grid.py
+++ corr_grid.py
@@ -46,6 +46,8 @@ def nsq_grid(dataset: Union[pd.DataFrame, str], x_vars: list, y_vars: list, log_
         delta_map[col] = (~upp_mask).astype(int)
         dataset.loc[upp_mask, col] = 2 * dataset[err_col][upp_mask]
 
+    grid_kws = {} if grid_kws is None else copy.copy(grid_kws)
+    regplot_kws = {} if regplot_kws is None else copy.copy(regplot_kws)
     g = SmartGrid(dataset, x_vars=x_vars, y_vars=y_vars, log_vars=log_vars, **grid_kws)
     g.map_offdiag(regplot_log_wrap, log_vars=log_vars, err_map=error_map, ranges_map=ranges_map,
                   delta_map=delta_map, data=dataset, linmix=True, **regplot_kws)
