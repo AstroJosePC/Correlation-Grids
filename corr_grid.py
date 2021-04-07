c796f58f5ff6f4a810484e38962178cd8c3ffef2
AstroJosePC <rayzote@email.arizona.edu>
2021-04-07 13:28:04 -0500

    :recycle: pass arguments differently

--- corr_grid.py
+++ corr_grid.py
@@ -2,7 +2,6 @@ import copy
 from typing import Optional
 from typing import Union
 
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
@@ -32,7 +31,7 @@ def regplot_log_wrap(x, y, log_vars: Optional[list] = None, err_map: Optional[di
 
 
 def nsq_grid(dataset: Union[pd.DataFrame, str], x_vars: list, y_vars: list, log_vars: Optional[list] = None,
-             grid_kws: Optional[dict] = None, regplot_kws: Optional[dict] = None):
+             regplot_kws: Optional[dict] = None, ann_coeff: bool = True, **kwargs):
     if isinstance(dataset, str):
         dataset = get_data(dataset)
     else:
@@ -47,9 +46,9 @@ def nsq_grid(dataset: Union[pd.DataFrame, str], x_vars: list, y_vars: list, log_
         delta_map[col] = (~upp_mask).astype(int)
         dataset.loc[upp_mask, col] = 2 * dataset[err_col][upp_mask]
 
-    grid_kws = {} if grid_kws is None else copy.copy(grid_kws)
     regplot_kws = {} if regplot_kws is None else copy.copy(regplot_kws)
-    g = SmartGrid(dataset, x_vars=x_vars, y_vars=y_vars, log_vars=log_vars, **grid_kws)
+    regplot_kws.setdefault('ann_coeff', ann_coeff)
+    g = SmartGrid(dataset, x_vars=x_vars, y_vars=y_vars, log_vars=log_vars, **kwargs)
     g.map_offdiag(regplot_log_wrap, log_vars=log_vars, err_map=error_map, ranges_map=ranges_map,
                   delta_map=delta_map, data=dataset, linmix=True, **regplot_kws)
     return g, error_map
