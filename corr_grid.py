ee3a07e3b0535b0fa55a5c837e8d6ba068ececde
AstroJosePC <rayzote@email.arizona.edu>
2021-04-12 22:45:02 -0500

    :art: overhaul on flux grids for luminosity convert

--- corr_grid.py
+++ corr_grid.py
@@ -57,8 +57,8 @@ def nsq_grid(dataset: Union[pd.DataFrame, str], x_vars: list, y_vars: list, log_
     return g, error_map
 
 
-def get_flux_upps(data: pd.DataFrame, error_map: Optional[dict] = None):
-    cols = identify_flux(data)
+def _flux_upps(data: pd.DataFrame, error_map: Optional[dict] = None, subset: Optional[list] = None):
+    cols = identify_flux(data, subset)
     error_map = parse_err_map(data, error_map, col_set=cols)
     for col in cols:
         err_col: str = error_map[col]
@@ -66,18 +66,56 @@ def get_flux_upps(data: pd.DataFrame, error_map: Optional[dict] = None):
         yield col, err_col, umask
 
 
-def flux_prep(data: pd.DataFrame, error_map: Optional[dict] = None):
+def _dist_col(data: pd.DataFrame):
+    indx = np.argmax([similar('dist', b.lower()) for b in data.columns])
+    return data.columns[indx]
+
+
+def flux2lum(flux: pd.Series, dist: pd.Series) -> pd.Series:
+    """
+    Calculate luminosity in solar luminosities.
+    The last term in the calculation is the unit conversion factor to Lsun.
+    :param flux: flux in units of erg / cm^2 / s
+    :param dist: distance in units of parsec
+    :return: luminosities in units of Lsun
+    """
+    return 4 * np.pi * flux * dist ** 2 * 2487.30567840084
+
+
+def flux_prep(data: pd.DataFrame, x_vars: list, y_vars: list, log_vars: list, error_map: Optional[dict] = None):
     """
     Pre-processing function for VISIR/Spitzer datasets
 
     :param data: dataset
+    :param x_vars:
+    :param y_vars:
+    :param log_vars:
     :param error_map: error map dictionary
     :return: delta map
     """
+    # Dictionary to store and map delta arrays
     dmap = {}
-    for col, err_col, upp_mask in get_flux_upps(data, error_map):
-        dmap[col] = (~upp_mask).astype(int)
+    # Get column that looks like distance
+    dist = _dist_col(data)
+    for col, err_col, upp_mask in _flux_upps(data, error_map, x_vars + y_vars):
+        # Store logical NOT of upper limit mask as delta array for linmix
+        dmap[f'lum_{col}'] = (~upp_mask).astype(int)
+        # Assign upper limits from error column into data column using upp_mask
         data.loc[upp_mask, col] = 2 * data[err_col][upp_mask]
+
+        # Calculate luminosity of line flux and set to data
+        data[f'lum_{col}'] = flux2lum(data[col], data[dist])
+        # Set luminosity error values to data
+        # Since we usually have no errors for distance we can't propagate errors, so we assume distance is a scalar
+        # TODO: Change method to at least search for distance errors!
+        data[f'lum_{col}_err'] = flux2lum(data[err_col], data[dist])
+
+        # Replace flux variables with luminosities to plot on grid
+        if col in x_vars:
+            x_vars[x_vars.index(col)] = f'lum_{col}'
+        if col in y_vars:
+            y_vars[y_vars.index(col)] = f'lum_{col}'
+        log_vars.append(f'lum_{col}')
     return dmap
 
 
@@ -89,6 +127,12 @@ def identify_flux(data: pd.DataFrame, subset: Optional[list] = None) -> List[str
 
 
 def _is_flux(col: str):
+    """
+    Test weather col is a string that looks like a name given to a flux data column
+    This test function is rather crude so I should expect bugs in the future
+    :param col: string to test
+    :return: True if test looks like a flux data column, False otherwise
+    """
     cl = col.lower()
     return 'err' not in cl and 'error' not in cl and cl.startswith('fl') or 'flux' in cl
 
@@ -109,8 +153,12 @@ def fluxes_grid(dataset: Union[pd.DataFrame, str], x_vars: list, y_vars: list, l
     else:
         dataset = dataset.copy()
 
-    # Data Prep
-    delta_map = flux_prep(dataset)
+    # Copy x_vars and y_var lists in case they can modified by flux_prep
+    x_vars = copy_obj(x_vars)
+    y_vars = copy_obj(y_vars)
+
+    # Flux Data Prep
+    delta_map = flux_prep(dataset, x_vars, y_vars, log_vars)
     # Generate Grid
     nsq_grid(dataset, x_vars, y_vars, log_vars=log_vars, delta_map=delta_map,
              regplot_kws=regplot_kws, ann_coeff=ann_coeff, copy=False, **kwargs)
